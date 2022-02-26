b2247dd703ef6579e5cbc046c5691049a41663c4
MR-Addcit <2750417853@qq.com>
2022-02-26 18:49:53 +0800

    Update Files

---
+++ Code/LEDBoard21x7/LEDBoard21x7.ino
@@ -0,0 +1,33 @@
+#include <BluetoothSerial.h>
+#include <FastLED.h>
+#include <RTClib.h>
+
+#include "HeaderConfig.h"
+#include "LEDFunction.h"
+
+void setup() {
+    pinMode(Button1, INPUT_PULLUP);
+    pinMode(Button2, INPUT_PULLUP);
+    pinMode(Button3, INPUT_PULLUP);
+    pinMode(Button4, INPUT_PULLUP);
+
+    // 设置蓝牙名称
+    SerialBT.begin("LED Board01");
+    rtc.begin();
+    if (rtc.lostPower())
+        rtc.adjust(DateTime(2021, 10, 20, 12, 30, 0));
+    DateTime now = rtc.now();
+    YEAR = now.year(), MONTH = now.month(), DATE = now.day(), HOUR = now.hour(),
+    MINUTE = now.minute();
+
+    FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS)
+        .setCorrection(TypicalSMD5050);
+    FastLED.setBrightness(BRIGHTNESS);
+    FastLED.clear();
+}
+
+void loop() {
+    Get_Button();
+    Get_Serial();
+    EVERY_N_MILLISECONDS(20) Display();
+}
